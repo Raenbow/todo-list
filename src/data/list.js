a5c57df9152f6fac37b677c6dde52be5b9aa8da7
delray <d.a.faskey@gmail.com>
2018-06-20 16:13:02 -0700

    Completed displaying dummy to-do data

---
+++ src/data/list.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        title: 'Wash Car',
+        details: 'Clean inside and outside',
+        id: '001'
+    },
+    {
+        title: 'Feed Cookie',
+        details: 'Cat needz cheesburger',
+        id: '002'
+    },
+    {
+        title: 'Feed Dog',
+        details: 'You dont own a dog',
+        id: '003'
+    },
+    {
+        title: 'Watch TV',
+        details: 'Relax girl, you do you',
+        id: '004'
+    },
+    {
+        title: 'Brush Hair',
+        details: 'No thanks',
+        id: '005'
+    }
+];
