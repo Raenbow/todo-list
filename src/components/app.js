2b1da2f926a537b9e3ad0c960a75df6f38289138
delray <d.a.faskey@gmail.com>
2018-06-20 18:04:14 -0700

    finished add item functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 //import 'bootstrap/dist/css/bootstrap.min.css';  **After installing bootstrap**
 import List from './list';
+import AddItem from './add_item';
+import listData from '../data/list';
 
-const App = () => (
-    <div className="container">
-        <h1 className="center">To-Do List</h1>
-        <List/>
-    </div>
-);
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            list: []
+        };
+    }
+
+    getListData(){
+        //Make call to server to get data
+
+        this.setState({
+            list: listData
+        });
+    }
+
+    componentDidMount(){
+        this.getListData();
+    }
+
+    addItem(item){
+        this.setState({
+            list: [item, ...this.state.list]
+        });
+    }
+
+    render(){
+        console.log('APP STATE:', this.state);
+        return (
+            <div className="container">
+                <h1 className="center">To-Do List</h1>
+                <AddItem add={this.addItem.bind(this)}/>
+                <List data={this.state.list}/>
+            </div>
+        );
+    }
+}
 
 export default App;
